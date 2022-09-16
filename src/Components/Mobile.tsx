174e0bab315d2a191f58f37641e6d46b221c463a
Robert597 <robertseun1@gmail.com>
2022-09-16 16:21:21 +0100

    navbar

--- src/Components/Mobile.tsx
+++ src/Components/Mobile.tsx
@@ -1,7 +1,10 @@
 import React, {useEffect, useRef} from 'react';
 import gsap from 'gsap';
 import "../Styles/mobile.css";
-import {FaTimes} from "react-icons/fa";
+import { BsArrowUpRight } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
+
+
  
 type state = {
     state: {
@@ -13,18 +16,36 @@ type state = {
 }
 
 const Mobile = ({state, handleMenu}: state ) => {
-const [close, setClose] = React.useState<Boolean>(true);
+const navigate = useNavigate();
 let menu = useRef<HTMLDivElement>(null);
-let secondaryMenu = useRef();
     useEffect(() => {
      if(state.clicked === false){
 //close
-gsap.to(".menuLink p span", {
-    y: -100,
-    duration: .5,
-    stagger: .2,
-    ease: "slow (0.7, 0.7, false)"
+gsap.to(".menulinkname", {
+        y: 120,
+        duration: 1,
+        ease: "power3.inOut"
+});
+gsap.to(".menuSocialsContainer h1 span", {
+    y: 120,
+    duration: 1,
+    ease: "power3.inOut"
 })
+gsap.to(".menuContactMe",  {
+    opacity: 0,
+    duration: 1,
+    ease: "power3.inOut"
+})
+gsap.to(".menuBottomContainer p", {
+    opacity: 0,
+    duration: 1,
+    ease: "power3.inOut"
+});
+gsap.to(".topContent", {
+    opacity: 0,
+    duration: 1,
+    ease: "power3.inOut"
+});
 gsap.to([".hamburger-menuu", ".menu-secondary"], {
     height: 0,
     delay: 1,
@@ -59,13 +80,46 @@ gsap.fromTo([".menu-secondary", ".hamburger-menuu"], {
     },
     ease: "power3.inOut"
 })
-gsap.fromTo(".menuLink p span", {
-    y: 100
+gsap.fromTo(".menulinkname", {
+    y: 120
 }, {
     y: 0,
-    duration: .8,
-    delay: .2,
+    duration: 1,
+    delay: 1,
     stagger: .2,
+    ease: "slow (0.7, 0.7, false)"
+})
+gsap.to(".topContent", {
+    opacity: 1,
+    duration: 1,
+    ease: "slow (0.7, 0.7, false)"
+});
+gsap.fromTo(".menuSocialsContainer h1 span", {
+    y: 120
+}, {
+    y: 0,
+    duration: 1,
+    delay: 1,
+    stagger: .2,
+    ease: "slow (0.7, 0.7, false)"
+})
+gsap.fromTo(".menuContactMe", {
+    opacity: 0,
+    scale: 1.4
+}, {
+    opacity: 1,
+    scale: 1,
+    duration: .8,
+    delay: 1.2,
+    ease: "slow (0.7, 0.7, false)"
+})
+gsap.fromTo(".menuBottomContainer p", {
+    opacity: 0,
+}, {
+    opacity: 1,
+    duration: 1,
+    delay: 1,
+    stagger: .4,
     ease: "slow (0.7, 0.7, false)"
 })
         }
@@ -76,34 +130,79 @@ gsap.fromTo(".menuLink p span", {
         <div className="menu-secondary">
         </div>
         <div className="menu-layer" >
-        <div className="menuu">
-        <div className="logoContainer">
+            <div className="topContent">
+            <div className="logoContainer">
              <h1 className='logo'>
-            ROBERT
+                <span className='logoname'>ROBERT</span>
+                <span className="logoCircle"></span>
             </h1>
         </div>
+         
+<div className='hamburger-menu' onClick={() => handleMenu()} >
+<span>Close</span>
+</div>
+            </div>
 
-        <div onClick={() => handleMenu()}>
-            <FaTimes/>
-        </div>
-        </div>
-            <div className="menu-container">
-                <div className="menu-wrapper">
-                    <div className="menu-links">
-                        <nav>
-                           <div className='menuLink'>
-                            <p onClick={() => handleMenu()}><span>Home</span></p>
-                            <p onClick={() => handleMenu()}><span>Projects</span></p>
-                            <p onClick={() => handleMenu()}><span>About</span></p>
-                            <p onClick={() => handleMenu()}><span>Contact</span></p>
-                           
-                        </div> 
-                        </nav>
-
-                    </div>
+            <div className="menuCenterContent">
+                <div className="menuLinkContainer">
+                    <h1 className="menuLink" onClick={() => {
+                        handleMenu()
+                        navigate("/home")
+                    }}>
+                        <span className="menulinkno">01.</span>
+                        <span className="menulinkname">Home</span>
+                    </h1>
+                    <h1 className="menuLink"
+                    onClick={() => {
+                        handleMenu()
+                        navigate("/about")
+                    }}>
+                        <span className="menulinkno">02.</span>
+                        <span className="menulinkname">About</span>
+                    </h1>
+                    <h1 className="menuLink"
+                    onClick={() => {
+                        handleMenu()
+                        navigate("/projects")
+                    }}>
+                        <span className="menulinkno">03.</span>
+                        <span className="menulinkname">Projects</span>
+                    </h1>
+                </div>
+                <div className="menuSocialsContainer">
+                <h1>
+                        <span>
+                        <a href="mailto:robertseun1@gmail.com">Ma.il <BsArrowUpRight/></a></span>
+                    </h1>
+                    <h1>
+                        <span> <a href="https://twitter.com/Rohbert001" target="_blank" rel="noreferrer">Twitter <BsArrowUpRight/></a> </span>
+                    </h1>
+                    <h1>
+                        <span> <a href="https://www.linkedin.com/in/oluwaseun-robert-a43b3221b" target="_blank" rel="noreferrer">LinkedIn <BsArrowUpRight/></a> </span>
+                    </h1>
+                    <h1>
+                        <span><a href="https://github.com/Robert597" target="_blank" rel="noreferrer">Github <BsArrowUpRight/></a></span>
+                    </h1>
                 </div>
             </div>
+
+            <div className="menuBottomContainer">
+                <p>
+                    <span>Available for work worldwide</span>
+                </p>
+                <p>
+                    <span>{new Date().toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', hour12: true})} - Lagos, Ng.</span>
+                </p>
+                <p>
+                    <span>Robert Folio &copy;2022.</span>
+                </p>
+            </div>
+
+            <div className="menuContactMe">
+                <p>Say Hi</p>
+            </div>
         </div>
+        
     </div>
   )
 }
